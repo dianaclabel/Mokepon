6b278af7d4c61043194be92853f5a2fa1958f108
Diana Clabel <dianaclabel08@gmail.com>
2022-10-27 14:37:08 -0500

    se agrega de forma aleatoria al enemigo junto con tu nombre

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -4,20 +4,44 @@ function iniciarJuego() {
 }
 
 function seleccionarMascotaJugador() {
-  let inputHipodoge = document.getElementById("hipodoge").checked;
-  let inputCapipepo = document.getElementById("capipepo").checked;
-  let inputRatigueya = document.getElementById("ratigueya").checked;
+  let inputHipodoge = document.getElementById("hipodoge");
+  let inputCapipepo = document.getElementById("capipepo");
+  let inputRatigueya = document.getElementById("ratigueya");
+  let spanMascotaJugador = document.getElementById("mascota-jugador");
+
+  let seleccionHecha = true;
 
   if (inputHipodoge.checked) {
-    alert("Seleccionó hipodoge ");
+    spanMascotaJugador.innerHTML = "Hipodoge";
   } else if (inputCapipepo.checked) {
-    alert("Seleccionó capipepo ");
+    spanMascotaJugador.innerHTML = "Capipepo";
   } else if (inputRatigueya.checked) {
-    alert("Seleccionó ratigueya");
+    spanMascotaJugador.innerHTML = "Ratigueya";
   } else {
-    alert("Seleccioné una laternativa");
+    alert("Seleccione una alternativa");
+    seleccionHecha = false;
+  }
+
+  if (seleccionHecha) {
+    seleccionarMascotaEnemigo();
   }
 }
 
+function seleccionarMascotaEnemigo() {
+  let spanMascotaEnemigo = document.getElementById("mascota-enemigo");
+  let ataqueAleatorio = aleatorio(1, 3);
+
+  if (ataqueAleatorio === 1) {
+    spanMascotaEnemigo.innerHTML = "Hipodoge";
+  } else if (ataqueAleatorio === 2) {
+    spanMascotaEnemigo.innerHTML = "Capipepo";
+  } else {
+    spanMascotaEnemigo.innerHTML = "ratigueya";
+  }
+}
+
+function aleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 //al objeto window le agregamos un escuchador donde va a disparar una funcion cuando toda nuestra pagina HTML alla cargado
 window.addEventListener("load", iniciarJuego);
